8d2afeed9236b882a6d0f79de4b6ecd1b23c3187
John Stoermer <jstoerm@purdue.edu>
2019-04-20 12:59:16 -0400

    socketio is going crazy rn

--- Chat-Room-BR/MainTest/test/src/components/Layout.js
+++ Chat-Room-BR/MainTest/test/src/components/Layout.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client'
 
+const socketUrl = "http://10.186.126.125:3231/"
 export default class Layout extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      socket:null
+    };
+  }
+
+  componentWillMount() {
+    this.initSocket()
+  }
+
+  initSocket = ()=>{
+    const socket = io(socketUrl)
+    socket.on('connect', ()=> {
+      console.log("connected");
+    })
+    this.setState({socket})
+  }
+
   render() {
     const { title } = this.props
     return (
