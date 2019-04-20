21568b5d245295426a32a86eeade9fcd9e457f80
John Stoermer <jstoerm@purdue.edu>
2019-04-20 11:58:53 -0400

    added some stuff that doesnt work

--- Chat-Room-BR/Frontend/src/components/Chat/index.js
+++ Chat-Room-BR/Frontend/src/components/Chat/index.js
@@ -1,18 +1,32 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+import React from 'react';
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+const Chat = () => (
+  <div>
+    <div>
+      <meta charSet="UTF-8"/>
+      <title>Socket.IO Chat Example</title>
+      <link rel="stylesheet" href="style.css"/>
+    </div>
+    <div>
+      <ul className="pages">
+        <li className="chat page">
+          <div className="chatArea">
+            <ul className="messages"></ul>
+          </div>
+          <input className="inputMessage" placeholder="Type here..."/>
+        </li>
+        <li className="login page">
+          <div className="form">
+            <h3 className="title">What's your nickname?</h3>
+            <input className="usernameInput" type="text" maxLength="14"/>
+          </div>
+        </li>
+      </ul>
+      <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+      <script src="/socket.io/socket.io.js"></script>
+      <script src="/main.js"></script>
+    </div>
+  </div>
+);
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+export default Chat;
