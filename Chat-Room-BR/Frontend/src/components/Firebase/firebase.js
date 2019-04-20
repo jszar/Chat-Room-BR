bfb732cbcda02c9f36528a33a632643ef11df108
jszar <jszarzyn@purdue.edu>
2019-04-19 22:59:41 -0400

    Set up Sign In for users with firebase

--- Chat-Room-BR/Frontend/src/components/Firebase/firebase.js
+++ Chat-Room-BR/Frontend/src/components/Firebase/firebase.js
@@ -1,4 +1,6 @@
 import app from 'firebase/app';
+import 'firebase/auth';
+
 
 const config = {
   apiKey: "AIzaSyD6JkIh_TuMySG0hHSPRgE9Uu6IW3-HJeA",
@@ -12,7 +14,21 @@ const config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+
+    this.auth = app.auth();
   }
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
