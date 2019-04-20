f63a76f241672e56d3aee98f65c709eb8c7ee219
jszar <jszarzyn@purdue.edu>
2019-04-20 00:56:05 -0400

    Changed sign out ot take you back to login

--- Chat-Room-BR/Frontend/src/components/SignOut/index.js
+++ Chat-Room-BR/Frontend/src/components/SignOut/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 import { withFirebase } from '../Firebase';
 
 const SignOutButton = ({ firebase }) => (
-  <button type="button" onClick={firebase.doSignOut} href="http://localhost:3000/">
+  <a type="button" onClick={firebase.doSignOut} href="http://localhost:3000/">
     Sign Out
-  </button>
+  </a>
 );
 
 export default withFirebase(SignOutButton);
