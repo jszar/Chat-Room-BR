b6e5b07681dd0c1ec5ec2c633860ba99ba95bad6
John Stoermer <jstoerm@purdue.edu>
2019-04-20 15:43:27 -0400

    fixed bug with conditional statement

--- Chat-Room-BR/MainTest/test/src/components/chats/ChatContainer.js
+++ Chat-Room-BR/MainTest/test/src/components/chats/ChatContainer.js
@@ -7,7 +7,7 @@ export default class ChatContainer extends Component {
 
     this.state = {
       chats:[],
-      activeChat:null
+      activeChat:null,
     };
   }
 
@@ -30,44 +30,50 @@ export default class ChatContainer extends Component {
   }
 
   render() {
-    const { user, logout } = this.props
-    const { activeChat, chats } = this.state
-    return (
-      <div className="container">
-        <SideBar
-          logout={logout}
-          chats={chats}
-          user={user}
-          activeChat={activeChat}
-          setActiveChat={this.setActiveChat}
-          />
-          <div className="chat-room-container">
-            {
-              activeChat !== null ? (
-                <div className="chat-room">
-                  <ChatHeading name={activeChat.name} />
-                  <Messages
-                  messages={activeChat.messages}
-                  user={user}
-                  typingUsers={activeChat.typingUsers}
-                  />
-                  <MessageInput
-                    sendMessage={
-                      (message)=>{
-                        this.sendMessage(activeChat.id, message)
-                      }
-                    }
-                    sendTyping={
-                      (isTyping) => {
-                        this.sendTyping(activeChat.id, isTyping)
-                      }
-                    }
-                    />
-                </div>
-              )
-            }
-          </div>
-      </div>
-    );
-  }
+		const { user, logout } = this.props
+		const { activeChat, chats } = this.state
+		return (
+			<div className="container">
+				<SideBar
+					logout={logout}
+					chats={chats}
+					user={user}
+					activeChat={activeChat}
+					setActiveChat={ (chat)=> this.setActiveChat(chat) }/>
+
+				<div className="chat-room-container">
+					{
+						activeChat !== null ? (
+							<div className="chat-room">
+								<ChatHeading
+									name={activeChat.name}
+									online={true} />
+								<Messages
+									messages={activeChat.messages}
+									user={user}
+									typingUsers={activeChat.typingUsers}/>
+
+								<MessageInput
+									sendMessage={
+										(message)=>{
+											this.sendMessage(activeChat.id, message)
+										}
+									}
+									sendTyping={
+										(isTyping)=>{
+											this.sendTyping(activeChat.id, isTyping)
+										}
+									}
+									/>
+							</div>
+            )
+            :
+							<div className="chat-room choose">
+								<h3>Choose a chat</h3>
+							</div>
+					}
+				</div>
+			</div>
+		);
+	}
 }
