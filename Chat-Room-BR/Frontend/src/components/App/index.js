49d0cf46a15bcf04657593dd394333b2bab6bb33
jszar <jszarzyn@purdue.edu>
2019-04-19 21:53:22 -0400

    Adds Routing

--- Chat-Room-BR/Frontend/src/components/App/index.js
+++ Chat-Room-BR/Frontend/src/components/App/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navigation from '../Navigation';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import ChatPage from '../Chat';
+import AccountPage from '../Account';
+import * as ROUTES from '../../constants/routes';
 
 const App = () => (
+  <Router>
   <div>
-    <h1>App</h1>
+    <Navigation />
+    <hr />
+    <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+    <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+    <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+    <Route path={ROUTES.HOME} component={HomePage} />
+    <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+    <Route path={ROUTES.CHAT} component={ChatPage} />
   </div>
+  </Router>
 );
 
 export default App;
