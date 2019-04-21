84ee7e632671fba038213025efaf2b7a3d56671f
jszar <jszarzyn@purdue.edu>
2019-04-21 16:45:50 -0400

    Navigation updated for when chat room is closed, authentication is now broken

--- Chat-Room-BR/Frontend/src/components/Navigation/index.js
+++ Chat-Room-BR/Frontend/src/components/Navigation/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 import { AuthUserContext } from '../Session';
-
+import { withFirebase } from '../Firebase';
+//todo fix authentication
+const INITIAL_STATE = {
+  serverdata: "",
+  isOpen: true,
+};
 
 const Navigation = () => (
   <div>
@@ -15,22 +20,73 @@ const Navigation = () => (
   </div>
 );
 
-const NavigationAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.HOME}>Home</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.ACCOUNT}>Account</Link>
-    </li>
-    <li>
-    <Link to={ROUTES.CHAT}>Battle Royal</Link>
-    </li>
-    <li>
-      <SignOutButton />
-    </li>
-  </ul>
-);
+class NavigationAuth extends Component {
+  constructor() {
+    super();
+    this.state = { ...INITIAL_STATE };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    this.props.firebase.user("GAMEROOMCHAT").on('value', snapshot => {
+      console.log(snapshot.val());
+      this.setState({
+        serverdata: snapshot.val(),
+        loading: false,
+      });
+    });
+  }
+    render(){
+      console.log(authUser => authUser);
+      //todo add auth
+      if (true) {
+       //if (!this.state){
+       this.state.isOpen = this.state.serverdata.isOpen;
+     //}
+      if (this.state.isOpen) {
+        return (
+          <ul>
+            <li>
+              <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+            </li>
+            <li>
+              <Link to={ROUTES.HOME}>Home</Link>
+            </li>
+            <li>
+              <Link to={ROUTES.ACCOUNT}>Account</Link>
+            </li>
+            <li>
+            <Link to={ROUTES.CHAT}>Battle Royal</Link>
+            </li>
+            <li>
+              <SignOutButton />
+            </li>
+          </ul>
+      )
+    }
+    else {
+      return (
+        <ul>
+          <li>
+            <Link to={ROUTES.HOME}>Home</Link>
+          </li>
+          <li>
+            <Link to={ROUTES.ACCOUNT}>Account</Link>
+          </li>
+          <p>Battle Royale is not available (*sad fortnite dancing*)</p>
+          <li>
+            <SignOutButton />
+          </li>
+        </ul>
+      )
+    }
+  }
+  else {
+    //for auth
+  }
+  }
+}
 
 const NavigationNonAuth = () => (
   <ul>
@@ -40,4 +96,5 @@ const NavigationNonAuth = () => (
   </ul>
 );
 
-export default Navigation;
+export default withFirebase(NavigationAuth);
+export {Navigation}
