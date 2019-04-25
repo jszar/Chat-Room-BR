8d5851998d5e18db74d9b8587683bdab99cd0d92
John Stoermer <jstoerm@purdue.edu>
2019-04-24 22:25:00 -0400

    THANOS IS HERERERERERERERERERE (endgame spoilers inside)

--- Chat-Room-BR/Frontend/src/components/Win/index.js
+++ Chat-Room-BR/Frontend/src/components/Win/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './index.css';
+import thanos from './thanos.gif';
+import * as ROUTES from '../../constants/routes';
 
 const WinPage = () => (
-  <div>
-    <h1 class="center">You win!</h1>
+  <div class="center">
+    <h1>You win!</h1>
     <br/>
-    <a class="center" href={ROUTES.HOME}>Go Home</a>
+    <a href={ROUTES.HOME}>Go Home</a>
+    <img src={thanos} id="bg"/>
   </div>
 );
 
