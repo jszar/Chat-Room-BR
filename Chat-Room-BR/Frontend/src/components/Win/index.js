5821984249f899e7c50c1f50703cc17d95365d0b
jszar <jszarzyn@purdue.edu>
2019-04-24 20:03:08 -0400

    Thanos Gif Thanos Gif

--- Chat-Room-BR/Frontend/src/components/Win/index.js
+++ Chat-Room-BR/Frontend/src/components/Win/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import logo from './AliveNippyJunebug-max-1mb.gif'
 
 import { withAuthorization } from '../Session';
+
+
 
 const WinPage = () => (
   <div>
     <h1>WINNER WINNER CHICKEN VICTORY ROYALE</h1>
+    <img src={logo}/>
   </div>
 );
 
